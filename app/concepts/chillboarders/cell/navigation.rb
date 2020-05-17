ea1fe71061b94378a2bb989fcd37b7064a2d72a2
Philipp Winkler <philoc@freenet.de>
2020-05-17 13:56:37 +0200

    [FEATURE] add locale switcher

--- app/concepts/chillboarders/cell/navigation.rb
+++ app/concepts/chillboarders/cell/navigation.rb
@@ -28,6 +28,11 @@ module Chillboarders
         { text: '.navigation.sign_in', path: :new_user_session, svg: 'svgs/login.svg' },
       ].freeze
 
+      LANGUAGES = [
+        { language: 'Deutsch', locale: 'de', flag: 'flags/germany.svg' },
+        { language: 'English', locale: 'en', flag: 'flags/england.svg' },
+      ].freeze
+
       def signed_in?
         options[:current_user].present?
       end
@@ -65,6 +70,30 @@ module Chillboarders
             end)
             out.concat content_tag(:h5, path[:text], class: 'd-xl-none d-inline')
           end
+        end
+      end
+
+      def switch_locale
+        actual_locale_data = LANGUAGES.find { |language| language[:locale] == I18n.locale.to_s }
+        content_tag(:div, class: 'locale-switcher') do
+          out =''
+          out.concat(content_tag(:button, class: 'btn btn-light', data: { toggle: 'dropdown' }) do
+            inner = ''
+            inner.concat image_tag(actual_locale_data[:flag], width: '15', height: '15')
+            inner.concat content_tag(:span, actual_locale_data[:locale].upcase, class: 'country-flag')
+            inner.concat content_tag(:i, 'arrow_drop_down', class: 'material-icons')
+          end)
+          out.concat(content_tag(:ul, class: 'dropdown-menu') do
+            LANGUAGES.map do |language|
+              content_tag(:li) do
+                link_to("/#{language[:locale]}") do
+                  inner = ''
+                  inner.concat image_tag(language[:flag], width: '15', height: '15')
+                  inner.concat content_tag(:p, language[:language])
+                end
+              end
+            end.join
+          end)
         end
       end
     end
