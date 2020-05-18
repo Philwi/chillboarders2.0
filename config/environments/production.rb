8a720ab4f948e74b9e189e065cfce1b3917b9112
Philipp Winkler <philoc@freenet.de>
2020-05-18 16:11:42 +0200

    [TASK] emails via mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,14 +108,13 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-      :address              => "smtp.sendgrid.net",
-      :port                 => 587,
-      :domain               => 'freenet.de',
-      :user_name            => ENV['SENDGRID_USERNAME'],
-      :password             => ENV['SENDGRID_PASSWORD'],
-      :authentication       => 'plain',
-      :enable_starttls_auto => true
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'chillboarders.heroku.com',
+    :authentication => :plain,
   }
   config.action_mailer.default_url_options = { :host => 'chillboarders.herokuapp.com' }
   config.active_storage.service = :amazon
