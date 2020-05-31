6ec43ad25af75afa90e403fcf09565780bf8ecc7
Philipp Winkler <philoc@freenet.de>
2020-05-31 22:28:26 +0200

    [TASK] create welcome message!

--- app/concepts/user/operation/create.rb
+++ app/concepts/user/operation/create.rb
@@ -9,6 +9,7 @@ module User::Operation
     step Contract::Validate(key: 'user')
     step Contract::Persist()
     step :create_default_user_site
+    step :send_welcome_message_to_user
 
     def create_default_user_site(ctx, model:, **)
       UserSite.create(
@@ -16,6 +17,16 @@ module User::Operation
         headline: I18n.t('.user_site.default_headline'),
         text: I18n.t('.user_site.default_text')
       )
+    end
+
+    def send_welcome_message_to_user(ctx, model:, **)
+      params = ActionController::Parameters.new({
+        user_message: {
+          body: I18n.t('.user_messages.welcome'),
+          for_user_id: model.id,
+        }
+      })
+      UserMessage::Operation::Create.(params: params, user: User.first)
     end
   end
 end
